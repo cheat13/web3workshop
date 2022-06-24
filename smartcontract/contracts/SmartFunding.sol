d0fba088b54888b57bebc532ee582e5a43bf1ae5
Kritsana <tragoolphrom@gmail.com>
2022-06-24 16:07:20 +0700

    implement refund & claim

--- smartcontract/contracts/SmartFunding.sol
+++ smartcontract/contracts/SmartFunding.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.15;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SmartFunding {
+    enum STAGE {
+        INACTIVE,
+        ACTIVE,
+        SUCESS,
+        FAILED
+    }
+    STAGE public fundingStages;
     address public tokenAddress;
     uint256 public goal;
     uint256 public pool;
@@ -11,19 +18,25 @@ contract SmartFunding {
 
     mapping(address => uint256) public investOf;
     mapping(address => uint256) public rewardOf;
+    mapping(address => bool) public claimedOf;
 
     event Invest(address indexed from, uint256 amount);
+    event ClaimReward(address indexed from, uint256 amount);
+    event Refund(address indexed from, uint256 amount);
 
     constructor(address _tokenAddress) {
         tokenAddress = _tokenAddress;
+        fundingStages = STAGE.INACTIVE;
     }
 
     function initialize(uint256 _goal, uint256 _endTimeInDay) external {
         goal = _goal;
         endTime = block.timestamp + (_endTimeInDay * 1 days);
+        fundingStages = STAGE.ACTIVE;
     }
 
     function invest() external payable {
+        require(fundingStages == STAGE.ACTIVE, "Funding is not active");
         require(block.timestamp < endTime, "Time is up");
         require(msg.value > 0, "Required amount of investment");
         require(msg.value <= goal, "Investment is too large");
@@ -31,12 +44,43 @@ contract SmartFunding {
 
         pool += msg.value;
         investOf[msg.sender] = msg.value;
-        rewardOf[msg.sender] = getReward();
+        rewardOf[msg.sender] = _getReward();
 
         emit Invest(msg.sender, msg.value);
     }
 
-    function getReward() private view returns (uint256) {
+    function refund() external {
+        require(fundingStages == STAGE.ACTIVE, "Funding is not active");
+        require(investOf[msg.sender] > 0, "No investment to refund");
+
+        uint256 invertAmount = investOf[msg.sender];
+        pool -= invertAmount;
+
+        delete investOf[msg.sender];
+        delete rewardOf[msg.sender];
+        delete claimedOf[msg.sender];
+
+        payable(msg.sender).transfer(invertAmount);
+
+        emit Refund(msg.sender, invertAmount);
+    }
+
+    function claim() external {
+        require(fundingStages == STAGE.SUCESS, "Funding is not successful");
+        require(!claimedOf[msg.sender], "Already claimed");
+        require(rewardOf[msg.sender] > 0, "No reward to claim");
+
+        uint256 reward = rewardOf[msg.sender];
+
+        rewardOf[msg.sender] = 0;
+        claimedOf[msg.sender] = true;
+
+        IERC20(tokenAddress).transfer(msg.sender, reward);
+
+        emit ClaimReward(msg.sender, reward);
+    }
+
+    function _getReward() private view returns (uint256) {
         uint256 totalSupply = IERC20(tokenAddress).totalSupply();
         return (totalSupply / goal) * msg.value;
     }
--- smartcontract/test/smartfunding-test.js
+++ smartcontract/test/smartfunding-test.js
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, waffle } = require("hardhat");
 const { utils } = ethers;
+const { provider } = waffle;
 
 const DECIMAL = 18;
 const TOTAL_SUPPLY = utils.parseUnits("1000000", DECIMAL);
@@ -70,11 +71,12 @@ describe("SmartFunding operations", function () {
 
   it("Should invest success", async function () {
     const tx1 = await fundingContract.connect(invester1).invest({ value: utils.parseEther("0.1") });
-    await tx1.wait();
     const tx2 = await fundingContract.connect(invester2).invest({ value: utils.parseEther("0.2") });
+    await tx1.wait();
     await tx2.wait();
 
     expect(await fundingContract.pool()).to.equal(utils.parseEther("0.3"));
+    expect(await provider.getBalance(fundingContract.address)).to.equal(utils.parseEther("0.3"));
 
     expect(await fundingContract.investOf(invester1.address)).to.equal(utils.parseEther("0.1"));
     expect(await fundingContract.investOf(invester2.address)).to.equal(utils.parseEther("0.2"));
@@ -86,8 +88,77 @@ describe("SmartFunding operations", function () {
     // expect(tx2).to.emit(fundingContract, "Invest").withArgs(invester2.address, utils.parseEther("0.1"));
   })
 
-  it("Should fail on invest 0", async function () {
-    const tx3 = fundingContract.connect(invester3).invest({ value: utils.parseEther("0") });
-    await expect(tx3).to.be.revertedWith("Required amount of investment");
+  it("Should invest fail", async function () {
+    const tx = fundingContract.connect(invester3).invest({ value: utils.parseEther("0") });
+    await expect(tx).to.be.revertedWith("Required amount of investment");
+  });
+
+  it("Should Claim reward success", async function () {
+    const tx1 = await fundingContract.connect(invester1).invest({ value: utils.parseEther("0.9") });
+    const tx2 = await fundingContract.connect(invester2).invest({ value: utils.parseEther("0.1") });
+    await tx1.wait();
+    await tx2.wait();
+
+    const txClaim1 = await fundingContract.connect(invester1).claim();
+    const txClaim2 = await fundingContract.connect(invester2).claim();
+    await txClaim1.wait();
+    await txClaim2.wait();
+
+    expect(await fundingContract.rewardOf(invester1.address)).to.equal(0);
+    expect(await fundingContract.rewardOf(invester2.address)).to.equal(0);
+
+    expect(await fundingContract.claimedOf(invester1.address)).to.equal(true);
+    expect(await fundingContract.claimedOf(invester2.address)).to.equal(true);
+
+    expect(await tokenContract.balanceOf(invester1.address)).to.equal(utils.parseUnits("900000", DECIMAL));
+    expect(await tokenContract.balanceOf(invester2.address)).to.equal(utils.parseUnits("100000", DECIMAL));
+    expect(await tokenContract.balanceOf(fundingContract.address)).to.equal(0);
+
+    // expect(tx1).to.emit(fundingContract, "ClaimReward").withArgs(invester1.address, utils.parseUnits("900000", DECIMAL));
+    // expect(tx1).to.emit(fundingContract, "ClaimReward").withArgs(invester2.address, utils.parseUnits("100000", DECIMAL));
+
+    // expect(tx1).to.emit(tokenContract, "Transfer").withArgs(fundingContract.address, invester1.address, utils.parseUnits("900000", DECIMAL));
+    // expect(tx2).to.emit(tokenContract, "Transfer").withArgs(fundingContract.address, invester2.address, utils.parseUnits("100000", DECIMAL));
+  });
+
+  it("Should Claim fail with 'No reward to claim'", async function () {
+    const tx = fundingContract.connect(invester3).claim();
+    await expect(tx).to.be.revertedWith("No reward to claim");
+  });
+
+  it("Should Claim fail with 'Already claimed'", async function () {
+    const tx = await fundingContract.connect(invester3).invest({ value: utils.parseEther("0.9") });
+    await tx.wait();
+
+    const txClaim = await fundingContract.connect(invester3).claim();
+    await txClaim.wait();
+
+    const txClaimAgain = fundingContract.connect(invester3).claim();
+    await expect(txClaimAgain).to.be.revertedWith("Already claimed");
+  });
+
+  if ("Shold Refund success", async function () {
+    const txInvest = await fundingContract.connect(invester1).invest({ value: utils.parseEther("0.1") });
+    await txInvest.wait();
+
+    const txRefund = await fundingContract.connect(invester1).refund();
+    await txRefund.wait();
+    
+    expect(await fundingContract.pool()).to.equal(0);
+    expect(await fundingContract.investOf(invester1.address)).to.equal(0);
+    expect(await fundingContract.rewardOf(invester1.address)).to.equal(0);
+    expect(await fundingContract.claimedOf(invester1.address)).to.equal(false);
+    expect(await provider.getBalance(fundingContract.address)).to.equal(0);
+  });
+
+  it("Should Claim fail with 'No investment to refund'", async function () {
+    const txInvest = await fundingContract.connect(invester1).invest({ value: utils.parseEther("0.1") });
+    await txInvest.wait();
+    
+    const txRefund = await fundingContract.connect(invester1).refund();
+    await txRefund.wait();
+
+    const txRefundAgain = fundingContract.connect(invester3).refund();
+    await expect(txRefundAgain).to.be.revertedWith("No investment to refund");
   });
 });
